aa7c5b7036763994aa691c979032902123273339
Erik Kuznetsov <erikkuz340@gmail.com>
2021-01-25 09:32:26 +0200

    Добавьте файлы проекта.

---
+++ slovar rus-ing/slovar_rus_ing.py
@@ -0,0 +1,19 @@
+def loe_failist(f):
+    fail=open(f,'r',encoding="utf-8-sig")
+    mas=[] 
+    for rida in fail:
+        mas.append(rida.strip())
+    fail.close()
+    return mas
+
+list=loe_failist("Rus.txt")
+
+def loe_failist(f):
+    fail=open(f,'r',encoding="utf-8-sig")
+    mas=[] 
+    for rida in fail:
+        mas.append(rida.strip())
+    fail.close()
+    return mas
+
+list=loe_failist("Ing.txt")
